227dac693368a0fe4f4eac6d6fba69d94753290a
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-25 20:10:18 +0530

    Pending edit todo functionality

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -26,10 +26,19 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => { 
             state.todos = state.todos.filter((todo)=>todo.id!==action.payload)
         },
-        EditTodo: (state, action) => { }
+        EditTodo: (state, action) => {
+            const{id , text} = action.payload;
+            console.log(id , text);
+ //      let editTodo= state.todos.filter((todo)=>todo.id === id )
+           const editTodo = state.todos.find((todo) => todo.id === id);
+           console.log(editTodo);
+           console.log(editTodo.text);
+
+           if(editTodo) editTodo.text=text
+         }
     }
 })
 
-export const {addTodo,removeTodo} = todoSlice.actions
+export const {addTodo,removeTodo,EditTodo} = todoSlice.actions
 
 export default todoSlice.reducer
